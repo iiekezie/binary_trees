83f88e96820bc6e1015d43ddcf9829c2046ac13b
iiekezie <106695537+iiekezie@users.noreply.github.com>
2024-06-07 16:38:02 +0100

    Binary Tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,6 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     {
         new->left = parent->left;
         parent->left->parent = new;
+        parent->left->parent = new;
     }
     parent->left = new;
 
