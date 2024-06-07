25c49e6729bebb9d74706e433ce3cdba28288841
iiekezie <106695537+iiekezie@users.noreply.github.com>
2024-06-07 18:05:34 +0100

    Binary Tree

---
+++ 132-array_to_heap.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - Builds a Max Binary Heap tree from an array
+ * @array: A pointer to the first element of the array to be converted
+ * @size: The number of elements in the array
+ *
+ * Return: A pointer to the root node of the created Binary Heap, or NULL on failure
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+    (void)array;
+    (void)size;
+    return (NULL); /* Placeholder */
+}
---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array of integers
+ * @heap: A pointer to the root node of the heap to convert
+ * @size: An address to store the size of the array
+ *
+ * Return: A pointer to the sorted array, or NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+    (void)heap;
+    (void)size;
+    return (NULL); /* Placeholder */
+}
